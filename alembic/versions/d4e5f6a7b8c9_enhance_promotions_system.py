70ea31ededcb06d1585f1787b22e89d45ba48533
pyPaulson <agyekumpaul07@gmail.com>
2026-08-21 17:23:48 +0000

    Fix: Update migration chain after promo migration removal
    - Changed d4e5f6a7b8c9 down_revision from c3d4e5f6a7b8 to z6a7b8c9d0e1
    - Reconnects migration chain after removing problematic promo migrations
    - Resolves KeyError during alembic upgrade
    
    Migration chain now valid and deployment can proceed.

--- alembic/versions/d4e5f6a7b8c9_enhance_promotions_system.py
+++ alembic/versions/d4e5f6a7b8c9_enhance_promotions_system.py
@@ -1,7 +1,7 @@
 """enhance promotions system
 
 Revision ID: d4e5f6a7b8c9
-Revises: c3d4e5f6a7b8
+Revises: z6a7b8c9d0e1
 Create Date: 2026-06-15
 
 """
@@ -13,7 +13,7 @@ from alembic import op
 from sqlalchemy.dialects import postgresql
 
 revision: str = "d4e5f6a7b8c9"
-down_revision: Union[str, None] = "c3d4e5f6a7b8"
+down_revision: Union[str, None] = "z6a7b8c9d0e1"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
